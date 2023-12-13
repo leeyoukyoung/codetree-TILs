92be065fd0f3b73aeb11283fe6985de9393746a3
leeyoukyoung <145317776+leeyoukyoung@users.noreply.github.com>
2023-12-13 18:16:08 +0900

    Title: 출석 부르기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231213/출석 부르기/calling-attendance.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    a=1;
+    b=2;
+    c=3;
+    if(a)
+    {
+        printf("John");
+    }
+    else if(b)
+    {
+        printf("Tom");
+    }
+    else if(c)
+    {
+        printf("Paul");
+    }
+    else
+    {
+        printf("Vacancy");
+    }
+    return 0;
+}
