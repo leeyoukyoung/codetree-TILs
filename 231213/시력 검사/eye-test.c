7d180769bf0121f5a74cc5e6463cd893c5cc49f0
leeyoukyoung <145317776+leeyoukyoung@users.noreply.github.com>
2023-12-13 20:37:55 +0900

    Title: 시력 검사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231213/시력 검사/eye-test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    float a,b;
+    scanf("%f\n%f",&a,&b);
+    if(a>=1.0&&b>=1.0)
+    {
+        printf("High");
+    }
+    else if(a>=0.5&&b>=0.5)
+    {
+        printf("Middle");
+    }
+    else
+    {
+        printf("Low");
+    }
+    return 0;
+}
